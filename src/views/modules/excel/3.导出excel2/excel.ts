961ac8d55263ab5cddf6441896df87a6a56c9bab
wangjunpeng <18438622750@163.com>
2024-10-11 13:21:58 +0800

    feat:导出excel方式2（自动调整列宽）

---
+++ src/views/modules/excel/3.导出excel2/excel.ts
@@ -0,0 +1,89 @@
+import * as XLSX from 'xlsx';
+import FileSaver from 'file-saver';
+import { formatDate } from '@/utils/time';
+import { deepClone } from '@/utils/tools';
+import { ElMessage } from 'element-plus';
+
+export function saveJsonToExcel(rawData, filename, columns) {
+	if (!rawData.length) return ElMessage.warning('数据为空，无法导出！');
+	let data = rawData;
+	if (columns && columns.length) {
+		data = deepClone(rawData).map((el) =>
+			columns.reduce((acc, col) => {
+				acc[col.label] = el[col.prop || col.field];
+				return acc;
+			}, {})
+		);
+	}
+	let sheet: any = XLSX.utils.json_to_sheet(data, { raw: true } as any);
+	let colWidths: any = [],
+		colNames = Object.keys(data[0]); // 所有列的名称数组
+
+	// 计算每一列的所有单元格宽度
+	// 先遍历行
+	data &&
+		data.forEach((row) => {
+			// 列序号
+			let index = 0;
+			// 遍历列
+			for (const key in row) {
+				if (colWidths[index] == null) colWidths[index] = [];
+
+				switch (typeof row[key]) {
+					case 'string':
+					case 'number':
+					case 'boolean':
+						colWidths[index].push(getCellWidth(row[key]));
+						break;
+					case 'object':
+					case 'function':
+						colWidths[index].push(0);
+						break;
+				}
+				index++;
+			}
+		});
+
+	sheet['!cols'] = [];
+	// 每一列取最大值最为列宽
+	colWidths.forEach((widths, index) => {
+		// 计算列头的宽度
+		widths.push(getCellWidth(colNames[index]));
+		// 设置最大值为列宽
+		sheet['!cols'].push({ wch: Math.max(...widths) });
+	});
+
+	let workbook = {
+		SheetNames: ['sheet1'],
+		Sheets: {
+			sheet1: sheet,
+		},
+	};
+
+	let wbout = XLSX.write(workbook, {
+		bookType: 'xlsx',
+		bookSST: true,
+		type: 'array',
+	});
+
+	FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), filename + formatDate(new Date(), 'YYYYMMDDHHmmss') + '.xlsx');
+
+	function getCellWidth(value) {
+		// 判断是否为null或undefined
+		if (value == null) {
+			return 10;
+		} else if (/.*[\u4e00-\u9fa5]+.*$/.test(value)) {
+			// 中文的长度
+			const chineseLength = value.match(/[\u4e00-\u9fa5]/g).length;
+			// 其他不是中文的长度
+			const otherLength = value.length - chineseLength;
+			return chineseLength * 2.1 + otherLength * 1.1;
+		} else {
+			return value.toString().length * 1.1;
+			/* 另一种方案
+			value = value.toString()
+			return value.replace(/[\u0391-\uFFE5]/g, 'aa').length
+			*/
+		}
+	}
+}
---
+++ src/views/modules/excel/3.导出excel2/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="2">
+		<el-form class="mb10">
+			<el-form-item>
+				<el-button type="primary" @click="exportExcel">导出Excel</el-button>
+			</el-form-item>
+		</el-form>
+
+		<el-table :data="tableData" border stripe>
+			<el-table-column v-for="item in columns" v-bind="item"></el-table-column>
+		</el-table>
+	</div>
+</template>
+
+<script setup lang="ts" name="导出Excel-2">
+import { list } from '@/api/table/base';
+import { saveJsonToExcel } from './excel';
+import { deepClone } from '@/utils/tools';
+
+// 定义变量
+const tableData = ref<any>([]);
+const columns = ref<any>([
+	{ label: '序号', prop: 'index', type: 'index', align: 'center', width: '60' },
+	{ label: '名称', prop: 'title', align: 'center', width: '', 'min-width': '' },
+	{ label: '描述', prop: 'desc', align: 'center', width: '', 'min-width': '' },
+	{ label: '时间', prop: 'createTime', align: 'center', width: '', 'min-width': '' },
+]);
+
+onBeforeMount(() => {
+	getData();
+});
+
+// 获取数据
+const getData = async () => {
+	const { data } = await list();
+	if (Array.isArray(data) && data.length) {
+		tableData.value = data;
+	} else {
+		tableData.value = [];
+	}
+};
+
+// 导出Excel
+const exportExcel = () => {
+	const data = deepClone(tableData.value).map((item: any, index: number) => {
+		item.index = index + 1;
+		return item;
+	});
+	saveJsonToExcel(data, '导出Excel测试.xlsx', columns.value);
+};
+</script>
+
+<style lang="scss" scoped></style>
